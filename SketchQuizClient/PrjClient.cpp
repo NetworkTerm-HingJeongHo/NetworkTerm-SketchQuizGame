393c2d67d684a3bd8e654b3e48fb5e6b9a34d0d6
Jiny <moc24@naver.com>
2023-12-03 18:00:15 +0900

    지안 : 중복확신 시 TCP 연결 스레드 추가(무한반복아님)
    지안 : 중복확신 시 TCP 연결 스레드 추가(무한반복아님)

--- SketchQuizClient/PrjClient.cpp
+++ SketchQuizClient/PrjClient.cpp
@@ -3,6 +3,7 @@
 // ---- 지안 변수 (로그인을 위함) ----- //
 _TCHAR input_result[256]; // input 결과 저장할 배열
 _TCHAR ID_NICKNAME[256]; // stdafx.h 파일에 같은 주소에 저장하기 위함
+HANDLE LoginProcessClientThread; // 로그인 프로세스 스레드, stdafx.h 파일에 같은 주소에 저장하기 위함
 
 // 홈 창 변수
 int channel;	//udp 채널 가져오기. stdafx.h 파일에 같은 주소에 저장하기 위함
@@ -597,7 +598,16 @@ LRESULT CALLBACK LoginWndProc(HWND hwndLogin, UINT msg, WPARAM wParam, LPARAM lP
 			// ---- 로그인 할때 TCP 연결 ---- //
 			_tcscpy(ID_NICKNAME, input_result); // 현재 입력한 ID 저장
 			WideCharToMultiByte(CP_ACP, 0, ID_NICKNAME, 256, NICKNAME_CHAR, 256, NULL, NULL); //_TCHAR 형 문자열을 char* 형 문자열로 변경
-			LoginProcessClient(); //TCP 연결. ->
+			// 스레드 생성!
+			//LoginProcessClient(); //TCP 연결. ->
+			// 스레드 생성
+			LoginProcessClientThread = CreateThread(NULL, 0, LoginProcessClient, NULL, 0, NULL);
+
+			//// 스레드 종료 대기
+			//WaitForSingleObject(LoginProcessClientThread, INFINITE);
+
+			//// 스레드 핸들 닫기
+			//CloseHandle(LoginProcessClientThread);
 			// ---------------------------- //
 			 
 			 
@@ -823,7 +833,7 @@ LRESULT CALLBACK Home_PassWndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lPa
 //--------------------------------------------------지안----------------------------------------------------------------//
 // 소켓 통신 스레드 함수 (0) - 로그인할때 소켓 통신하기
 // 클라이언트와 데이터 통신
-DWORD WINAPI LoginProcessClient()
+DWORD WINAPI LoginProcessClient(LPVOID arg)
 {
 
 	int retval;
@@ -840,19 +850,6 @@ DWORD WINAPI LoginProcessClient()
 	Loginserveraddr.sin_port = htons(SERVERPORT); //server에 대한 설정
 	retval = connect(g_sock, (SOCKADDR*)&Loginserveraddr, sizeof(Loginserveraddr)); //연결할때, 서버소켓 정보를 준다. -> establishied 상태
 	if (retval == SOCKET_ERROR) err_quit("connect()");
-
-	
-	
-	// 데이터 통신에 사용할 변수
-	//char buf[BUFSIZE + 1]="TCP연결해라잉";
-	//int len;
-
-	//retval = send(g_sock, buf, strlen(buf), 0); //버퍼사이즈로 보내기
-	//if (retval == SOCKET_ERROR) {
-	//	err_display("send()");
-	//	//break;
-	//}
-	
 
 	int len;
 	len = sizeof(NICKNAME_CHAR);
